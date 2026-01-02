37c8bf3942d9676e73bd8bd2d0c5bfe39ed922d6
harshN26 <hnihalani26@gmail.com>
2026-01-01 23:46:31 -0500

    update reset poses based on drive practice and update things to do

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -86,8 +86,8 @@ public class Constants {
     public static final Pose blueBackStartingPose=new Pose(55,9,Math.toRadians(90));
 
 
-    public static final Pose blueResetPose=new Pose(136,9,Math.toRadians(90));
-    public static final Pose redResetPose=new Pose(9,9,Math.toRadians(90));
+    public static final Pose blueResetPose=new Pose(136.25,9,Math.toRadians(90));
+    public static final Pose redResetPose=new Pose(7.75,9,Math.toRadians(90));
 
 
 
