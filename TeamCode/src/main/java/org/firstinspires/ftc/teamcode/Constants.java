e134d8ab226c8bfac1b33cd96241f0e22bab3130
harshN26 <hnihalani26@gmail.com>
2026-01-11 19:47:58 -0500

    1/11

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -109,7 +109,7 @@ public class Constants {
     public static final double filterR=5.0;
     public static final int filterN=3;
 
-    public static double turretOffsetX=1, turretOffsetY=-1;
+    public static double turretOffsetX=2.3125, turretOffsetY=-1.125;
 
 
 
