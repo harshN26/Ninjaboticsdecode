514446145dde8c6a10ae7352ce4856f8ae52eb27
harshN26 <hnihalani26@gmail.com>
2026-02-20 23:09:54 -0500

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -31,6 +31,7 @@ public class Constants {
             .translationalPIDFCoefficients(new PIDFCoefficients(0.07,0.000075,0.002,0.02))
             .headingPIDFCoefficients(new PIDFCoefficients(1.5,0.2,0.04,0.01))
             .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.01,0.0001,0.00001,0.7,0));
+    //.drivePIDFCoefficients(new FilteredPIDFCoefficients(0.001,0.005,0.0004,0.7,0));
 
 
     public static MecanumConstants driveConstants = new MecanumConstants()
