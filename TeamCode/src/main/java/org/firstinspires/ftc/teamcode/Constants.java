f22116d299b6a8304d973b1e1f25a014571779bf
harshN26 <hnihalani26@gmail.com>
2026-02-22 15:58:28 -0500

    color sensor shenanigans

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -114,7 +114,7 @@ public class Constants {
     public static double CGreenIN=2.3;
 
     public static double CPurpleOUT=0.95;
-    public static double CGreenOUT=1.68;
+    public static double CGreenOUT=1.7;
 
     public static double SOTM=0.005;
 
